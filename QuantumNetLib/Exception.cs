9d08695643407d4567b21a183f38c438394a5ceb
void-aeon <95763925+void-aeon@users.noreply.github.com>
2024-03-10 01:40:13 +0300

    Revert "Refactor Exception class and add derived exception classes"
    This reverts commit fb5300cd7620a007367bd1ad24ffd0a0b05d21e4.

--- QuantumNetLib/Exception.cs
+++ QuantumNetLib/Exception.cs
@@ -1,74 +1,19 @@
-﻿using System;
-
-namespace QuantumNetLib
+﻿namespace QuantumNetLib
 {
-    public class Exception : System.Exception
+    public class Exception
     {
-        public Exception(string message)
-        {
-            Message = message;
-        }
-
         public Exception(string message, int errorCode)
         {
             Message = message;
             ErrorCode = errorCode;
         }
 
-        public Exception(string message, Exception innerException)
-        {
-            Message = message;
-            InnerException = innerException;
-        }
-
-        public Exception(string message, int errorCode, Exception innerException)
-        {
-            Message = message;
-            ErrorCode = errorCode;
-            InnerException = innerException;
-        }
-
-        public string Message { get; }
-        public int ErrorCode { get; }
-        public Exception InnerException { get; }
-
-        public virtual string StackTrace => Environment.StackTrace;
+        public string Message { get; set; }
+        public int ErrorCode { get; set; }
 
         public override string ToString()
         {
-            var message = Message;
-            if (ErrorCode != 0)
-                message += $"\nError Code: {ErrorCode}";
-            if (InnerException != null)
-                message += $"\nInner Exception: {InnerException}";
-            return message;
+            return $"Error code: {ErrorCode}\nMessage: {Message}";
         }
-    }
-
-    public class ArgumentException : Exception
-    {
-        public ArgumentException(string message) : base(message) { }
-        public ArgumentException(string message, string innerException) : base(message, innerException) { }
-    }
-
-    public class ArgumentNullException : ArgumentException
-    {
-        public ArgumentNullException(string paramName) : base($"Argument '{paramName}' cannot be null.") { }
-    }
-
-    public class ArgumentOutOfRangeException : ArgumentException
-    {
-        public ArgumentOutOfRangeException(string paramName) : base($"Argument '{paramName}' is out of range.") { }
-    }
-
-    public class InvalidOperationException : Exception
-    {
-        public InvalidOperationException(string message) : base(message) { }
-        public InvalidOperationException(string message, Exception innerException) : base(message, innerException) { }
-    }
-
-    public class IndexOutOfRangeException : Exception
-    {
-        public IndexOutOfRangeException(string message = "Index is out of range.") : base(message) { }
     }
 }
