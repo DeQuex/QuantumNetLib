33418da5862d1b3b6c74e7d50dd8c1652b4e3230
void-aeon <95763925+void-aeon@users.noreply.github.com>
2024-03-10 01:40:23 +0300

    Revert "Refactor Math class to improve readability and add error handling"
    This reverts commit ebfe8c683b5481473a11c17de7e2f2ff228e0d03.

--- QuantumNetLib/Math.cs
+++ QuantumNetLib/Math.cs
@@ -1,144 +1,82 @@
 ﻿namespace QuantumNetLib
 {
-    public static class Math
+    public class Math
     {
-        public const float PI = 3.14159265359f;
-        public const float E = 2.71828182846f;
+        public static float PI = 3.14159265359f;
+        public static float E = 2.71828182846f;
 
-        public static bool IsEqual(float a, float b, float epsilon = 0.0001f)
+        public static float Abs(float a)
         {
-            return Abs(a - b) < epsilon;
-        }
-        public static bool IsZero(float value, float epsilon = 0.0001f)
-        {
-            return Abs(value) < epsilon;
+            return a < 0 ? -a : a;
         }
 
-        public static float Abs(float value)
+        public static float Sqrt(float a)
         {
-            return value < 0 ? -value : value;
-        }
-
-        public static float Sqrt(float value)
-        {
-            if (value < 0)
-                throw new ArgumentException("Value must be non-negative.");
-
-            float guess = value / 2;
-            float result = 0;
-
-            while (Abs(guess - result) > 0.0001f)
+            var x = a;
+            float y = 1;
+            const float e = 0.000001f;
+            while (x - y > e)
             {
-                result = guess;
-                guess = (guess + value / guess) / 2;
+                x = (x + y) / 2;
+                y = a / x;
             }
 
-            return result;
+            return x;
         }
 
-        public static float Pow(float baseValue, int exponent)
+        public static float Pow(float a, float b)
         {
-            if (exponent < 0)
-                throw new ArgumentException("Exponent must be non-negative.");
-
             float result = 1;
-            while (exponent != 0)
-            {
-                if ((exponent & 1) == 1)
-                    result *= baseValue;
-                baseValue *= baseValue;
-                exponent >>= 1;
-            }
+            for (var i = 0; i < b; i++) result *= a;
             return result;
         }
 
-        public static float Sin(float radians)
+        public static float Sin(float a)
         {
-            const int terms = 10;
             float result = 0;
-
-            for (int i = 0; i < terms; i++)
-            {
-                float term = Pow(-1, i) * Pow(radians, 2 * i + 1) / Factorial(2 * i + 1);
-                result += term;
-            }
-
+            for (var i = 0; i < 10; i++) result += Pow(-1, i) * Pow(a, 2 * i + 1) / Factorial(2 * i + 1);
             return result;
         }
 
-        public static float Cos(float radians)
+        public static float Cos(float a)
         {
-            const int terms = 10;
             float result = 0;
-
-            for (int i = 0; i < terms; i++)
-            {
-                float term = Pow(-1, i) * Pow(radians, 2 * i) / Factorial(2 * i);
-                result += term;
-            }
-
+            for (var i = 0; i < 10; i++) result += Pow(-1, i) * Pow(a, 2 * i) / Factorial(2 * i);
             return result;
         }
 
-        public static float Tan(float radians)
+        public static float Tan(float a)
         {
-            float cos = Cos(radians);
-            if (cos == 0)
-                throw new ArgumentException("Tangent is undefined for this angle.");
-
-            return Sin(radians) / cos;
+            return Sin(a) / Cos(a);
         }
 
-        public static int Factorial(int value)
+        public static float Factorial(float a)
         {
-            if (value < 0)
-                throw new ArgumentException("Value must be non-negative.");
-
-            int result = 1;
-            for (int i = 2; i <= value; i++)
-                result *= i;
+            float result = 1;
+            for (var i = 1; i <= a; i++) result *= i;
             return result;
         }
 
-        public static float Log(float value)
+        public static float Log(float a)
         {
-            if (value <= 0)
-                throw new ArgumentException("Value must be positive.");
-
-            const int terms = 100;
             float result = 0;
-
-            for (int i = 1; i < terms; i++)
-            {
-                float term = Pow(-1, i + 1) * Pow(value - 1, i) / i;
-                result += term;
-            }
-
+            for (var i = 1; i < 100; i++) result += Pow(-1, i + 1) * Pow(a - 1, i) / i;
             return result;
         }
 
-        public static float Log(float value, float baseValue)
+        public static float Log(float a, float b)
         {
-            if (value <= 0 || baseValue <= 0 || baseValue == 1)
-                throw new ArgumentException("Invalid input values.");
-
-            return Log(value) / Log(baseValue);
+            return Log(a) / Log(b);
         }
 
-        public static float Log10(float value)
+        public static float Log10(float a)
         {
-            if (value <= 0)
-                throw new ArgumentException("Value must be positive.");
-
-            return Log(value) / Log(10);
+            return Log(a) / Log(10);
         }
 
-        public static float Log2(float value)
+        public static float Log2(float a)
         {
-            if (value <= 0)
-                throw new ArgumentException("Value must be positive.");
-
-            return Log(value) / Log(2);
+            return Log(a) / Log(2);
         }
 
         public static float Min(float a, float b)
@@ -151,29 +89,30 @@
             return a > b ? a : b;
         }
 
-        public static float Clamp(float value, float min, float max)
+        public static float Clamp(float a, float min, float max)
         {
-            return value < min ? min : value > max ? max : value;
+            return a < min ? min : a > max ? max : a;
         }
 
         public static float Lerp(float a, float b, float t)
         {
-            return a + (b - a) * Clamp(t, 0, 1);
+            return a + (b - a) * t;
         }
 
-        public static float Map(float value, float fromMin, float fromMax, float toMin, float toMax)
+        public static float Map(float a, float min1, float max1, float min2, float max2)
         {
-            if (fromMin == fromMax)
-                throw new ArgumentException("From range is invalid.");
-
-            float t = (value - fromMin) / (fromMax - fromMin);
-            return Lerp(toMin, toMax, t);
+            return (a - min1) / (max1 - min1) * (max2 - min2) + min2;
         }
 
-        public static float RandomRange(float min, float max)
+        public static float Random(float min, float max)
         {
-            Random random = new Random();
-            return (float)(random.NextDouble() * (max - min) + min);
+            var random = new Random();
+            return random.NextFloat(min, max);
+        }
+
+        public static float Random(float max)
+        {
+            return Random(0, max);
         }
     }
 }
